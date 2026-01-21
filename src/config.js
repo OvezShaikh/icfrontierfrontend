99bd2b5e61eaf3eff6fc9597d27674a17a7d74e5
Ovez <ovezshaikh13052001@gmail.com>
2026-01-21 10:47:41 +0530

    added changes for deploying on server after mongo uri

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
-// src/config.js - BULLETPROOF API CONFIG
+// src/config.js - VITE SAFE API CONFIG
+
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 const config = {
-  API_URL: typeof process !== 'undefined' && process.env?.REACT_APP_API_URL 
-    ? process.env.REACT_APP_API_URL 
-    : 'http://localhost:5000'
+  API_URL,
 };
 
 export default config;
