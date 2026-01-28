a7b1c5c90706588a7dc7974214e813d6d1779787
Ovez <ovezshaikh13052001@gmail.com>
2026-01-28 18:11:03 +0530

    added /thankyou

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -26,7 +26,7 @@ export default function Contact() {
                 <input
                   type="hidden"
                   name="_next"
-                  value={`${import.meta.env.VITE_API_URL}/thankyou`}
+                  value={`/thankyou`}
                 />
 
                 <input
